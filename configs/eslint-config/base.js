5dfd5c703d825d322c174b77ce890a7f9d2ce510
Jardel Bordignon <jardel1101@gmail.com>
2025-03-19 20:39:10 -0300

    🎨 Configurando Eslint

--- configs/eslint-config/base.js
+++ configs/eslint-config/base.js
@@ -3,6 +3,9 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import importPlugin from 'eslint-plugin-import-x';
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "../prettier-config/index.mjs";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,9 +19,43 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      prettier: prettierPlugin,
+      import: importPlugin,
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+      "prettier/prettier": ["error", prettierConfig],
+      "no-console": "warn", // Evita logs desnecessários em produção
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }], // Ignora variáveis iniciadas com _
+      "eqeqeq": ["error", "always"], // Exige uso de === e !==
+      // "curly": "error", // Força uso de chaves em blocos condicionais
+      "prefer-const": "warn", // Sugere const em vez de let sempre que possível
+      // "@typescript-eslint/explicit-function-return-type": "warn", // Exige definição de tipo de retorno em funções
+      "@typescript-eslint/no-explicit-any": "warn", // Evita uso de `any`
+      "@typescript-eslint/no-non-null-assertion": "error", // Evita `!` em valores possivelmente `null`
+      "@typescript-eslint/consistent-type-imports": "error", // Garante imports consistentes de tipos
+      'import/order': [
+        'error',
+        {
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'object', 'type'],
+          'newlines-between': 'never',
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'before',
+            },
+          ],
+        },
+      ],
+      'sort-imports': [
+        'error',
+        {
+          allowSeparatedGroups: true,
+          ignoreDeclarationSort: true,
+        },
+      ],
     },
   },
   {
@@ -27,6 +64,6 @@ export const config = [
     },
   },
   {
-    ignores: ["dist/**"],
+    ignores: ["dist/**", "node_modules/**", "coverage/**", "*.config.js"],
   },
 ];
