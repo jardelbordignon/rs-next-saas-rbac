3bc6e6083ebc9825363b5ddcffe0c1961e979723
Jardel Bordignon <jardel1101@gmail.com>
2025-03-25 23:51:57 -0300

    ✨ Criando middleware de autenticação

--- apps/backend/src/http/domains/users/get-profile/get-profile.controller.ts
+++ apps/backend/src/http/domains/users/get-profile/get-profile.controller.ts
@@ -1,34 +1,38 @@
 import { z } from 'zod'
+import { auth } from '@/http/middlewares'
 import { getProfileService } from './get-profile.service'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 
 export async function getProfileController(fastify: FastifyInstance) {
-  fastify.withTypeProvider<ZodTypeProvider>().get(
-    '/profile',
-    {
-      schema: {
-        summary: 'Get authenticated user profile',
-        tags: ['Users'],
-        response: {
-          200: z.object({
-            user: z.object({
-              id: z.string(),
-              name: z.string().nullable(),
-              email: z.string(),
-              avatarUrl: z.string().url().nullable(),
+  fastify
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/profile',
+      {
+        schema: {
+          summary: 'Get authenticated user profile',
+          tags: ['Users'],
+          response: {
+            200: z.object({
+              user: z.object({
+                id: z.string(),
+                name: z.string().nullable(),
+                email: z.string(),
+                avatarUrl: z.string().url().nullable(),
+              }),
             }),
-          }),
-          404: z.object({
-            message: z.string().default('User not found'),
-          }),
+            404: z.object({
+              message: z.string().default('User not found'),
+            }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { sub } = await request.jwtVerify<{ sub: string }>()
-      const result = await getProfileService(sub)
-      return reply.status(200).send(result)
-    },
-  )
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const result = await getProfileService(userId)
+        return reply.status(200).send(result)
+      },
+    )
 }
