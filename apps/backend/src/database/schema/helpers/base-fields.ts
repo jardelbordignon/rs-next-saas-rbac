25e207e67cf3402cb9b02ea314c230f530ca2af8
Jardel Bordignon <jardel1101@gmail.com>
2025-05-12 17:52:03 -0300

    :recycle: base-fields

--- apps/backend/src/database/schema/helpers/base-fields.ts
+++ apps/backend/src/database/schema/helpers/base-fields.ts
@@ -2,18 +2,14 @@ import { createId } from '@paralleldrive/cuid2'
 import { timestamp, varchar } from 'drizzle-orm/pg-core'
 
 type Props = {
-  onlyId?: boolean
+  id?: boolean
+  createdAt?: boolean
   updatedAt?: boolean
   deleteAt?: boolean
 }
 
-type BaseFields = typeof fields & typeof optionals
-
 const fields = {
   id: varchar({ length: 30 }).$defaultFn(createId).primaryKey(),
-}
-
-const optionals = {
   createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
   updatedAt: timestamp('updated_at', { withTimezone: true }).$onUpdate(
     () => new Date(),
@@ -24,28 +20,27 @@ const optionals = {
 /**
  * Returns an object with base fields for a model.
  *
- * @param {boolean} [onlyId=false] - If set to true, only the id field is returned.
- * @param {boolean} [updatedAt=true] - If set to false, updatedAt field is not returned.
- * @param {boolean} [deletedAt=false] - If set to true, deletedAt field is returned.
+ * @param id - If set to true, id field is returned (default true).
+ * @param createdAt - If set to true, createdAt field is returned (default true).
+ * @param updatedAt - If set to true, updatedAt field is returned (default true).
+ * @param deletedAt - If set to true, deletedAt field is returned (default false).
  *
- * @returns {Record<string, import('drizzle-orm').PgColumn>} - An object with base fields.
+ * @returns - An object with base fields.
  */
 export const baseFields = ({
-  onlyId = false,
+  id = true,
+  createdAt = true,
   updatedAt = true,
   deleteAt = false,
 }: Props = {}) => {
-  if (!onlyId) {
-    Object.assign(fields, {
-      createdAt: optionals.createdAt,
-      ...(updatedAt && {
-        updatedAt: optionals.updatedAt,
-      }),
-      ...(deleteAt && {
-        deletedAt: optionals.deletedAt,
-      }),
-    })
-  }
+  const baseFields = {}
 
-  return fields as BaseFields
+  Object.assign(baseFields, {
+    ...(id ? { id: fields.id } : {}),
+    ...(createdAt ? { createdAt: fields.createdAt } : {}),
+    ...(updatedAt ? { updatedAt: fields.updatedAt } : {}),
+    ...(deleteAt ? { deletedAt: fields.deletedAt } : {}),
+  })
+
+  return baseFields as typeof fields
 }
