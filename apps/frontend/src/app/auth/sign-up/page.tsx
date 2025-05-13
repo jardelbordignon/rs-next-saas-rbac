61281b98b53b979adcd0f136d138c7e639c49468
Jardel Bordignon <jardel1101@gmail.com>
2025-05-13 20:50:31 -0300

    ✨ Página de cadastro

--- apps/frontend/src/app/auth/sign-up/page.tsx
+++ apps/frontend/src/app/auth/sign-up/page.tsx
@@ -3,9 +3,14 @@ import Link from 'next/link'
 import { facebookIcon, githubIcon, googleIcon } from '@/assets'
 import { Button, Input, Label, Separator } from '@/components/ui'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form action='' className='space-y-4'>
+      <div className='space-y-1'>
+        <Label htmlFor='name'>Name</Label>
+        <Input name='name' type='text' id='name' />
+      </div>
+
       <div className='space-y-1'>
         <Label htmlFor='email'>E-mail</Label>
         <Input name='email' type='email' id='email' />
@@ -14,36 +19,41 @@ export default function SignInPage() {
       <div className='space-y-1'>
         <Label htmlFor='password'>Password</Label>
         <Input name='password' type='password' id='password' />
+      </div>
 
-        <Link
-          href='/auth/forgot-password'
-          className='text-foreground text-xs font-medium hover:underline'
-        >
-          Forgot your password?
-        </Link>
+      <div className='space-y-1'>
+        <Label htmlFor='password_confirmation'>Password Confirmation</Label>
+        <Input name='password_confirmation' type='password' id='password' />
       </div>
 
       <Button type='submit' className='w-full'>
-        Sign in
+        Create account
       </Button>
 
       <Button variant='link' className='w-full' asChild>
-        <Link href='/auth/sign-up'>Not registered? Sign up</Link>
+        <Link href='/auth/sign-in'>Already registered? Sign in</Link>
       </Button>
+
+      {/* <Link
+          href='/auth/sign-in'
+          className='text-xs self-center font-medium text-foreground hover:underline'
+        >
+          I have an account
+      </Link> */}
 
       <Separator />
 
       <Button variant='outline' className='w-full'>
         <Image src={facebookIcon} alt='' className='mr-2 size-4 dark:invert' />
-        Sign in with Facebook
+        Sign up with Facebook
       </Button>
       <Button variant='outline' className='w-full'>
         <Image src={googleIcon} alt='' className='mr-2 size-4 dark:invert' />
-        Sign in with Google
+        Sign up with Google
       </Button>
       <Button variant='outline' className='w-full'>
         <Image src={githubIcon} alt='' className='mr-2 size-4 dark:invert' />
-        Sign in with Github
+        Sign up with Github
       </Button>
     </form>
   )
