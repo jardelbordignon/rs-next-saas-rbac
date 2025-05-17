9c0f9983e714a431f3cb76fd7755f2ef50664a17
Jardel Bordignon <jardel1101@gmail.com>
2025-05-17 10:47:24 -0300

    ✨ Permissão para criar projeto

--- apps/frontend/src/app/(app)/org/[orgSlug]/create-project/page.tsx
+++ apps/frontend/src/app/(app)/org/[orgSlug]/create-project/page.tsx
@@ -1,7 +1,15 @@
+import { redirect } from 'next/navigation'
+import { ability } from '@/auth/auth'
 import { Header } from '@/components/layout'
 import { ProjectForm } from './form'
 
-export default function CreateProject() {
+export default async function CreateProject() {
+  const permissions = await ability()
+
+  if (permissions?.cannot('create', 'Project')) {
+    redirect('/')
+  }
+
   return (
     <div className='space-y-4'>
       <Header />
