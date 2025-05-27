dae5086c24c52167e295fb18743d5d7c7b774d77
Jardel Bordignon <jardel1101@gmail.com>
2025-05-26 22:39:59 -0300

    🐛 Ajuste para não travar no lint durante a build

--- apps/frontend/src/app/auth/enter-with-social-account/actions.ts
+++ apps/frontend/src/app/auth/enter-with-social-account/actions.ts
@@ -1,11 +1,12 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
 'use server'
 
 import { redirect } from 'next/navigation'
 
 export async function enterWithFacebook() {
   const url = new URL('https://www.facebook.com/v13.0/dialog/oauth')
-  url.searchParams.set('client_id', process.env.FACEBOOK_CLIENT_ID)
-  url.searchParams.set('redirect_uri', process.env.FACEBOOK_REDIRECT_URI)
+  url.searchParams.set('client_id', process.env.FACEBOOK_CLIENT_ID ?? '')
+  url.searchParams.set('redirect_uri', process.env.FACEBOOK_REDIRECT_URI ?? '')
   url.searchParams.set('scope', 'email')
 
   redirect(url.toString())
@@ -13,8 +14,8 @@ export async function enterWithFacebook() {
 
 export async function enterWithGithub() {
   const url = new URL('https://github.com/login/oauth/authorize')
-  url.searchParams.set('client_id', process.env.GITHUB_CLIENT_ID)
-  url.searchParams.set('redirect_uri', process.env.GITHUB_REDIRECT_URI)
+  url.searchParams.set('client_id', process.env.GITHUB_CLIENT_ID ?? '')
+  url.searchParams.set('redirect_uri', process.env.GITHUB_REDIRECT_URI ?? '')
   url.searchParams.set('scope', 'user:email')
 
   redirect(url.toString())
@@ -22,8 +23,8 @@ export async function enterWithGithub() {
 
 export async function enterWithGoogle() {
   const url = new URL('https://accounts.google.com/o/oauth2/v2/auth')
-  url.searchParams.set('client_id', process.env.GOOGLE_CLIENT_ID)
-  url.searchParams.set('redirect_uri', process.env.GOOGLE_REDIRECT_URI)
+  url.searchParams.set('client_id', process.env.GOOGLE_CLIENT_ID ?? '')
+  url.searchParams.set('redirect_uri', process.env.GOOGLE_REDIRECT_URI ?? '')
   url.searchParams.set('response_type', 'code')
   url.searchParams.set('scope', 'profile email')
 
