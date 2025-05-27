439dacdbd8e5a93e8213f7b58122dfd12914906b
Jardel Bordignon <jardel1101@gmail.com>
2025-05-26 23:55:33 -0300

    🐛 Ajuste sign-out redirect

--- apps/frontend/src/app/api/auth/sign-out/route.ts
+++ apps/frontend/src/app/api/auth/sign-out/route.ts
@@ -1,14 +1,13 @@
 import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { redirect } from 'next/navigation'
+//import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
   const { delete: remove } = await cookies()
   remove('accessToken')
 
-  const redirectUrl = request.nextUrl.clone()
-  redirectUrl.pathname = '/auth/sign-in'
-  redirectUrl.search = ''
-
-  return NextResponse.redirect(redirectUrl)
+  //return NextResponse.redirect(new URL('/auth/sign-in', request.url))
+  //return NextResponse.redirect('/auth/sign-in')
+  redirect('/auth/sign-in')
 }
